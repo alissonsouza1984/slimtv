1a796746b2741a96136d0e6cb03ab8e71ac9f599
alissonsouza1984 <alissonseginf@gmail.com>
2025-12-04 18:21:29 -0300

    add index

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="refresh" content="0; URL='http://caderno.online'" />
+    <title>Redirecionando...</title>
+</head>
+<body>
+    <p>Redirecionando...</p>
+</body>
+</html>
+
